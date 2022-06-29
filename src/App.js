93a354d0a297957bfe099cf701a4c6c7eda2283b
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-06-29 02:02:43 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header'
+function App() {
+  const name='marouane'
+  const x=true
+  return (
+    <div className="container">
+      <Header title="Task tracker"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+
+export default function Header(props) {
+  return (
+    <header>
+        <h1>{props.title}</h1>
+    </header>
+  )
+}
