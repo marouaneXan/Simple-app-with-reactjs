deec665e62719dbacc7aec0b28cb2e14f683130f
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-06-30 13:53:46 +0100

    add component Task.js

---
+++ src/components/Task.js
@@ -0,0 +1,9 @@
+const Task = () => {
+  return (
+    <div className="task">
+        <h3>My Task</h3>
+    </div>
+  )
+}
+export default Task
+
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import Task from './Task'
 const Tasks = (props) => {
   return (
     <div>
       {props.tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task}/>
       ))}
     </div>
   )
