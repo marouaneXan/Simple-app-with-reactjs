855531e3956b902a8ef877702fa1821930ecd1ef
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-06-30 13:45:58 +0100

    some change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import { useState } from 'react'
 function App() {
+  const [tasks,setTasks]=useState(
+    [
+        {
+          id: 1,
+          text: "marouane zahaoui",
+          day: 2022,
+          reminder: true,
+        },
+        {
+          id: 2,
+          text: "ahmed roufani",
+          day: 2022,
+          reminder: true,
+        },
+        {
+          id: 1,
+          text: "aymane squirro",
+          day: 2022,
+          reminder: true,
+        },
+      ]
+)
   return (
     <div className="container">
       <Header title="Task tracker"/>
-      <Tasks/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,32 +1,11 @@
-import {useState} from 'react'
-export default function Tasks() {
-    const [tasks,setTasks]=useState(
-        [
-            {
-              id: 1,
-              text: "marouane zahaoui",
-              day: 2022,
-              reminder: true,
-            },
-            {
-              id: 2,
-              text: "ahmed roufani",
-              day: 2022,
-              reminder: true,
-            },
-            {
-              id: 1,
-              text: "aymane squirro",
-              day: 2022,
-              reminder: true,
-            },
-          ]
-    )
+import React from 'react'
+const Tasks = (props) => {
   return (
     <div>
-      {tasks.map((task) => (
+      {props.tasks.map((task) => (
         <h3 key={task.id}>{task.text}</h3>
       ))}
     </div>
-  );
+  )
 }
+export default Tasks
