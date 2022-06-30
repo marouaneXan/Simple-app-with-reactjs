5abc9ba1ac378999515519454ba455ed788e8fd4
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-06-30 14:05:30 +0100

    add FaTimes icon in Tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,12 @@
+import { FaTimes } from "react-icons/fa";
 const Task = (props) => {
   return (
     <div className="task">
-        <h3>{props.task.text}</h3>
-        <h3>{props.task.day}</h3>
+      <h3>
+        {props.task.text} <FaTimes style={{color:'red'}}/>
+      </h3>
+      <h3>{props.task.day}</h3>
     </div>
-  )
-}
-export default Task
-
+  );
+};
+export default Task;
